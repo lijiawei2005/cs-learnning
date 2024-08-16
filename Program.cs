0ff05494a9f10e87aaf663d660c9327a33f93dfa
武汉大学 <161034209+lijiawei2005@users.noreply.github.com>
2024-08-16 20:22:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    abstract class Father
+    {
+        public abstract void   hanshu();//申明一个抽象方法
+
+    }
+    class Son : Father
+    {
+        public override void hanshu()
+        {
+            
+        }//子类必须实现父类的抽象方法
+    }
+
+    class Program {
+    //抽象类不能被实例化
+    
+    }
+
+}
